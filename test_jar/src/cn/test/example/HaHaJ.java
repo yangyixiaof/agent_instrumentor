254afc12f411767922447b7d23679a008faaf68c
DELL <DELL@DESKTOP-20DEPKI>
2017-09-19 11:47:59 +0800

    small change.

---
+++ test_jar/src/cn/test/example/HaHaJ.java
@@ -0,0 +1,22 @@
+package cn.test.example;
+
+public class HaHaJ {
+	
+	public HaHaJ() {
+	}
+	
+	public void test2(int x, int y) {
+		if (x > y) {
+			System.out.println("Great!");
+		}
+	}
+	
+	public static void main(String[] args) {
+		int x = 0;
+		if (args.length > x) {
+			System.err.println("hahah");
+		}
+		new HaHaJ().test2(x, 100);
+	}
+	
+}
