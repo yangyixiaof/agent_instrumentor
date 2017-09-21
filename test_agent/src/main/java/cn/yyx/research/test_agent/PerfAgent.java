2cf7db00def9fa03d066ada0d03d294022fbcb84
yyx <yyx@DESKTOP-6AH7GPK>
2017-09-21 11:10:59 +0800

    small change.

--- test_agent/src/main/java/cn/yyx/research/test_agent/PerfAgent.java
+++ test_agent/src/main/java/cn/yyx/research/test_agent/PerfAgent.java
@@ -1,6 +1,8 @@
 package cn.yyx.research.test_agent;
 
 import java.lang.instrument.Instrumentation;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Hello world!
@@ -10,6 +12,12 @@ public class PerfAgent {
 
 	public static void premain(String agentArgs, Instrumentation _inst) {
 		System.out.println("agentArgs:" + agentArgs);
+		List<String> filters = new LinkedList<String>();
+		if (agentArgs != null && !agentArgs.equals("")) {
+			String[] agents = agentArgs.split(";|:|#");
+			
+		}
+		
 		CmpTransformer trans = new CmpTransformer();
 		_inst.addTransformer(trans);
 	}
