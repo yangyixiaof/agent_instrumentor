a207b345fdd4cd31d9ddcb53eba6c1db646efe27
yyx <yyx@ubuntu>
2017-12-29 00:14:29 -0800

    small change.

--- test_agent/src/main/java/cn/yyx/research/test_agent/PerfAgent.java
+++ test_agent/src/main/java/cn/yyx/research/test_agent/PerfAgent.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class PerfAgent {
 
 	public static void premain(String agentArgs, Instrumentation _inst) {
-		System.out.println("Received Filters:" + ((agentArgs == null || agentArgs.equals("")) ? "@no filters!" : agentArgs));
+		System.out.println("Received Filters:" + ((agentArgs == null || agentArgs.equals("")) ? "@no filters/flowers!" : agentArgs));
 		List<String> flowers = new LinkedList<String>();
 		if (agentArgs != null && !agentArgs.equals("")) {
 			String[] agents = agentArgs.split(";|:|#");
