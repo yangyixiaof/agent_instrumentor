1ced871e37b669ccc9d2ca48a9e5be57f4b97a9d
yyx <yyx@ubuntu>
2018-01-10 00:51:35 -0800

    small change.

--- test_agent/src/main/java/cn/yyx/research/common_mode/InstrumentJar.java
+++ test_agent/src/main/java/cn/yyx/research/common_mode/InstrumentJar.java
@@ -6,6 +6,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.ProcessBuilder.Redirect;
 import java.util.Iterator;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
@@ -45,7 +46,7 @@ public class InstrumentJar {
 					if (!ze.isDirectory()) {
 						String norm_name = ze.getName().replace('\\', '/');
 						if (!norm_name.contains("/") && norm_name.endsWith("classes.dex")) {
-							System.err.println("file - " + norm_name + "; " + ze.getName() + " : " + ze.getSize() + " bytes");
+							System.out.println("file - " + norm_name + "; " + ze.getName() + " : " + ze.getSize() + " bytes");
 							InputStream is = zf.getInputStream(ze);
 							String unzipped_dex_file_path = dex_work_dir + "/" + norm_name;
 							FileOutputStream fos = new FileOutputStream(new File(unzipped_dex_file_path));
@@ -64,6 +65,8 @@ public class InstrumentJar {
 							{
 								ProcessBuilder pb = new ProcessBuilder("d2j-dex2jar.sh", norm_name);
 								pb.directory(new File(dex_work_dir));
+								pb.redirectOutput(Redirect.INHERIT);
+								pb.redirectError(Redirect.INHERIT);
 								try {
 									Process p = pb.start();
 									p.waitFor();
@@ -79,10 +82,12 @@ public class InstrumentJar {
 							String jar_name = raw_norm_name + "-dex2jar.jar";
 							InstrumentOneJar(dex_work_dir + "/" + jar_name, 100);
 							
-							System.err.println("generate instrumented dex:d2j-jar2dex.sh " + jar_name);
+							System.out.println("generate instrumented dex:d2j-jar2dex.sh " + jar_name);
 							{
 								ProcessBuilder pb = new ProcessBuilder("d2j-jar2dex.sh", jar_name);
 								pb.directory(new File(dex_work_dir));
+								pb.redirectOutput(Redirect.INHERIT);
+								pb.redirectError(Redirect.INHERIT);
 								try {
 									Process p = pb.start();
 									p.waitFor();
@@ -121,8 +126,8 @@ public class InstrumentJar {
 	public static void InstrumentOneJar(String jar_file_path, int max_instrument_file_num) {
 		File jar_file = new File(jar_file_path);
 		String jar_name = jar_file.getName();
-		System.err.println("Depackaging " + jar_name);
-
+		
+		System.out.println("Deleting existing directory:" + work_dir);
 		File dir = new File(work_dir);
 		if (dir.exists()) {
 			FileUtil.DeleteFile(dir);
@@ -131,9 +136,12 @@ public class InstrumentJar {
 		File new_jar_path = new File(work_dir + "/" + jar_name);
 		FileUtil.CopyFile(new File(jar_file_path), new_jar_path);
 		
+		System.out.println("Depackaging " + jar_name);
 		{
 			ProcessBuilder pb = new ProcessBuilder("jar", "xvf", jar_name);
 			pb.directory(new File(work_dir));
+			pb.redirectOutput(Redirect.INHERIT);
+			pb.redirectError(Redirect.INHERIT);
 			try {
 				Process p = pb.start();
 				p.waitFor();
@@ -144,18 +152,18 @@ public class InstrumentJar {
 			}
 		}
 		
-		System.err.println("Deleting " + new_jar_path);
+		System.out.println("Deleting " + new_jar_path);
 		FileUtil.DeleteFile(new_jar_path);
 		
 		File wdir = new File(work_dir);
 		String wdir_abs_path = wdir.getAbsolutePath();
 		String wdir_abs_path_norm = wdir_abs_path.replace('\\', '/');
-		FileIterator fi = new FileIterator(work_dir, "*.class");
+		FileIterator fi = new FileIterator(work_dir, ".*\\.class");
 		Iterator<File> fi_itr = fi.EachFileIterator();
 		int count = 0;
 		while (fi_itr.hasNext()) {
 			File class_f = fi_itr.next();
-			System.err.println("Instrumenting " + class_f.getName());
+			System.out.println("Instrumenting " + class_f.getName());
 			try {
 				byte[] bytes = FileUtil.ReadBytesFromFile(class_f);
 				String classname = class_f.getAbsolutePath().replace('\\', '/').substring(wdir_abs_path_norm.length());
@@ -169,11 +177,11 @@ public class InstrumentJar {
 			}
 			count++;
 			if (max_instrument_file_num >= 0 && count >= max_instrument_file_num) {
-				System.err.println("We will stop soon!");
+				System.out.println("We will stop soon!");
 				break;
 			}
 		}
-		System.err.println("Repackaging " + jar_name);
+		System.out.println("Repackaging " + jar_name);
 		{
 			ProcessBuilder pb = new ProcessBuilder("jar", "cvf", jar_name, ".");
 			pb.directory(new File(work_dir));
@@ -186,7 +194,7 @@ public class InstrumentJar {
 				e.printStackTrace();
 			}
 		}
-		System.err.println("Replacing original jar:" + jar_file_path);
+		System.out.println("Replacing original jar:" + jar_file_path);
 		FileUtil.CopyFile(new File(work_dir + "/" + jar_name), new File(jar_file_path));
 	}
 
