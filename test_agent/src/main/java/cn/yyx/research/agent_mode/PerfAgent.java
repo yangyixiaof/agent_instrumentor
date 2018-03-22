02401b9a96513355bbd596ac0dfa52ee151b658a
yixiao yang <yangyixiaofirst@163.com>
2018-03-22 17:07:06 +0800

    small change.

--- test_agent/src/main/java/cn/yyx/research/agent_mode/PerfAgent.java
+++ test_agent/src/main/java/cn/yyx/research/agent_mode/PerfAgent.java
@@ -5,6 +5,8 @@ import java.lang.instrument.Instrumentation;
 import java.util.LinkedList;
 import java.util.List;
 
+import cn.yyx.research.trace_recorder.TraceRecorder;
+
 /**
  * Hello world!
  *
@@ -12,15 +14,16 @@ import java.util.List;
 public class PerfAgent {
 
 	public static void premain(String agentArgs, Instrumentation _inst) {
-		System.out.println("Received Filters:" + ((agentArgs == null || agentArgs.equals("")) ? "@no filters/flowers!" : agentArgs));
+//		System.out.println("Received Filters:" + ((agentArgs == null || agentArgs.equals("")) ? "@no filters/flowers!" : agentArgs));
+		System.out.println("RecordDirectoryPath:" + agentArgs);
+		TraceRecorder.trace_dir = agentArgs;
 		List<String> flowers = new LinkedList<String>();
-		if (agentArgs != null && !agentArgs.equals("")) {
-			String[] agents = agentArgs.split(";|:|#");
-			for (String agent : agents) {
-				flowers.add(agent.replace('.', '/'));
-			}
-		}
-		
+//		if (agentArgs != null && !agentArgs.equals("")) {
+//			String[] agents = agentArgs.split(";|:|#");
+//			for (String agent : agents) {
+//				flowers.add(agent.replace('.', '/'));
+//			}
+//		}
 		ClassFileTransformer trans = new InverseCmpTransformer(flowers); // CmpTransformer
 		_inst.addTransformer(trans);
 	}
