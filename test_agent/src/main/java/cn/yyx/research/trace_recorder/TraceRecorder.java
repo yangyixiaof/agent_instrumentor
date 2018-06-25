12fd654f4576bb176f79a4c065cd7968e511a92e
SnowOnion <snowonionlee@gmail.com>
2018-06-25 13:36:23 +0800

    formated code, to make merging clean

--- test_agent/src/main/java/cn/yyx/research/trace_recorder/TraceRecorder.java
+++ test_agent/src/main/java/cn/yyx/research/trace_recorder/TraceRecorder.java
@@ -4,70 +4,70 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-/**
- * Hello world!
- *
- */
+/** Hello world! */
 public class TraceRecorder {
-	
-	public static String trace_dir;
-//	= System.getProperty("user.home") + "/" + "trace.txt";
 
-	private static StringBuffer buffer = new StringBuffer();
-	static {
-		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-			public void run() {
-				Flush();
-			}
-		}));
-	}
-	
-	public static void Append(Object x) {
-		buffer.append(x.getClass().getName() + "#" + x == null ? "null" : x.toString() + "#");
-	}
-	
-	public static void Append(String x) {
-		buffer.append(x + "#");
-	}
-	
-	public static void Append(int x) {
-		buffer.append(x + "#");
-	}
-	
-	public static void Append(float x) {
-		buffer.append(x + "#");
-	}
-	
-	public static void Append(long x) {
-		buffer.append(x + "#");
-	}
-	
-	public static void Append(double x) {
-		buffer.append(x + "#");
-	}
-	
-	public static void NewLine() {
-		buffer.append(System.getProperty("line.separator"));
-	}
-	
-	public static void Flush() {
-		File f = new File(trace_dir + "/" + "trace.txt");
-		
-		FileWriter writer = null;
-		try {
-			writer = new FileWriter(f);
-			writer.write(buffer.toString());
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (writer != null) {
-				try {
-					writer.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
-	
+  public static String trace_dir;
+  //	= System.getProperty("user.home") + "/" + "trace.txt";
+
+  private static StringBuffer buffer = new StringBuffer();
+
+  static {
+    Runtime.getRuntime()
+        .addShutdownHook(
+            new Thread(
+                new Runnable() {
+                  public void run() {
+                    Flush();
+                  }
+                }));
+  }
+
+  public static void Append(Object x) {
+    buffer.append(x.getClass().getName() + "#" + x == null ? "null" : x.toString() + "#");
+  }
+
+  public static void Append(String x) {
+    buffer.append(x + "#");
+  }
+
+  public static void Append(int x) {
+    buffer.append(x + "#");
+  }
+
+  public static void Append(float x) {
+    buffer.append(x + "#");
+  }
+
+  public static void Append(long x) {
+    buffer.append(x + "#");
+  }
+
+  public static void Append(double x) {
+    buffer.append(x + "#");
+  }
+
+  public static void NewLine() {
+    buffer.append(System.getProperty("line.separator"));
+  }
+
+  public static void Flush() {
+    File f = new File(trace_dir + "/" + "trace.txt");
+
+    FileWriter writer = null;
+    try {
+      writer = new FileWriter(f);
+      writer.write(buffer.toString());
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      if (writer != null) {
+        try {
+          writer.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+  }
 }
