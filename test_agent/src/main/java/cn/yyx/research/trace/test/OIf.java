b4ec12c68d43b64f9bf4c292e0647f9556eab76b
yangy <yangy@DESKTOP-BC9KLPC>
2018-07-05 09:20:24 +0800

    small change.

--- test_agent/src/main/java/cn/yyx/research/trace/test/OIf.java
+++ test_agent/src/main/java/cn/yyx/research/trace/test/OIf.java
@@ -2,13 +2,12 @@ package cn.yyx.research.trace.test;
 
 public class OIf {
 	
-	public Object ReturnObject() {
+	public static Object ReturnObject() {
 		return null;
 	}
 	
 	public static void main(String[] args) {
-		OIf oif = new OIf();
-		Object o = oif.ReturnObject();
+		Object o = OIf.ReturnObject();
 		if (o == null) {
 			System.out.println("null judgement executed!");
 		}
