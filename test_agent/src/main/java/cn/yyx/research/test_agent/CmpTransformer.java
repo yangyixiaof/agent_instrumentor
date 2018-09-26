734c9b47e7bc5aec6eb2376a8d8d63e8239d690b
yangy <yangy@DESKTOP-BC9KLPC>
2018-09-26 15:23:16 +0800

    small change.

--- test_agent/src/main/java/cn/yyx/research/test_agent/CmpTransformer.java
+++ test_agent/src/main/java/cn/yyx/research/test_agent/CmpTransformer.java
@@ -18,8 +18,8 @@ public class CmpTransformer implements ClassFileTransformer {
 	public CmpTransformer() {
 		// set up flowers
 //		flowers.add("java/lang/String");
-		flowers.add("randoop/generation/date/test/StaticSimpleBranchFile");
-		flowers.add("cn/yyx/research/trace/test/");
+		flowers.add("randoop/generation/date/test/");
+		flowers.add("randoop/generation/date/test/resource/");
 		// set up forbids
 		forbids.add("cern/colt/");
 		forbids.add("cn/yyx/");
